1ff9269d6191bd990ea18ff29984177735a63181
Ulises <ulivego@hotmail.com>
2022-03-18 08:50:55 -0600

    Feat(App): Testing Bear

--- LERT_App/App.tsx
+++ LERT_App/App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+type CustomTextPropTypes = {
+  name?: string
+}
+/**
+ * Oso text
+ * @param name nombre del oso 
+ * @returns 
+ */
+const CustomText = (props: CustomTextPropTypes) => {
+  return (
+    <Text>{props.name}</Text>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <CustomText name = "oso"/>
       <StatusBar style="auto" />
     </View>
   );
