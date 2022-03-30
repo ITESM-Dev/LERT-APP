b3a78bb427b8697227c5f89bb7969d099cc08052
RafaGomezGlez <A01733983@itesm.mx>
2022-03-30 12:50:27 -0600

    feat: Creation of doomie screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image 
+                style = {{height: '100%', width: '100%'}}
+                source ={{uri:'https://tentulogo.com/wp-content/uploads/2017/08/ibm-logo.jpg'}}
+            />
+            <Image 
+                style = {{height: '100%', width: '100%'}}
+                source ={require('./src/assets/ITESMDev.png')}
+            />
+            <Text
+                style = {{fontSize: 80  }}
+            > 
+                Testing Hello world!  
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
