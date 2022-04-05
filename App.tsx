c3855af376a92a817bab3c2a6dc18e98d05b26de
Ulises <ulivego@hotmail.com>
2022-04-04 22:35:59 -0500

    (ref):added logos and changed title
    So far I only added IBM's logo and our squad logo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{flex: 2, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+        <Image 
+          style={styles.logo}
+          source={require('./assets/ItesmDev_Transparente.png')}
+        />
+        <Image 
+          style={{width: 320, height: 128, padding: 10}}
+          source={require('./assets/IBM_logo.png')}
+        />
+      </View>
+      <View style={{flex: 1}}>
+        <Text style={styles.title}>ItesmDev's LERT Prototype</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +28,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 320,
+    height: 160,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#00367d"
   },
 });
