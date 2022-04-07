df662f47828987e539b52d666410510f699d718f
GSLeonardo <leo_galindo_01@hotmail.com>
2022-04-07 11:04:48 -0500

    wip(LertButton): Creating Component LERT-92

--- src/components/atoms/LertButton.tsx
+++ src/components/atoms/LertButton.tsx
@@ -2,17 +2,71 @@ import { Button } from 'native-base'
 
 type LertButtonPropTypes = {
     title: string;
+    type: string;
 }
 
+/**
+ * @param title Text for Button
+ * @param type "primary" | "secondary" | "terciary" | "danger" | "ghost"
+ */
 const LertButton = (props: LertButtonPropTypes) => {
-
-    const { title } = props
-
+    
+    const { title, type } = props
+    
     return (
-        <Button>
+        <Button 
+        // Style
+        {...LertButtonStyles[type]} 
+        // Hover Style
+        _hover={LertButtonStyles[`${type}Hover`]} 
+        borderRadius={0} 
+        >
             {title}
         </Button>
     );
 };
+
+type StyleType = {
+    variant?: string;
+    backgroundColor?: string;
+    borderColor?: string;
+    color?: string;
+}
+
+// Button Styles
+const LertButtonStyles: { [name: string]: StyleType} = {
+    primary: {
+        backgroundColor: "actions.actionsPrimary",
+    },
+    primaryHover: {
+
+    },
+    secondary: {
+        backgroundColor: "alerts.errorSecondary",
+    },
+    secondaryHover: {
+
+    },
+    terciary: {
+        borderColor: "actions.actionsPrimary",
+        variant: 'outline',
+    },
+    terciaryHover : {
+        backgroundColor: "actions.actionsPrimary",
+        variant: 'outline',
+    },
+    danger: {
+        backgroundColor: "alerts.errorSecondary",
+    },
+    dangerHover: {
+
+    },
+    ghost: {
+        variant: 'ghost',
+    },
+    ghostHover: {
+
+    }
+}
 
 export default LertButton;
