6aac7e479d82c01bae6c8f81670422a36460b198
GSLeonardo <leo_galindo_01@hotmail.com>
2022-04-08 12:01:16 -0500

    ref(App): Fonts LERT-92

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
 import { View, StyleSheet } from 'react-native';
 
 import { NativeBaseProvider } from 'native-base';
@@ -6,6 +9,18 @@ import theme from '~theme/theme';
 import Tests from 'Tests';
 
 export default function App() {
+
+	let [fontsLoaded] = useFonts({
+		"IBMPlexSans-Bold": require("./assets/fonts/IBMPlexSans-Bold.ttf"),
+		"IBMPlexSans-SemiBold": require("./assets/fonts/IBMPlexSans-SemiBold.ttf"),
+		"IBMPlexSans-Light": require("./assets/fonts/IBMPlexSans-Light.ttf"),
+		"IBMPlexSans-Regular": require("./assets/fonts/IBMPlexSans-Regular.ttf")
+	});
+
+	if(!fontsLoaded) {
+		return <AppLoading />;
+	}
+	
 	return (
 		<NativeBaseProvider theme={theme}>
 			<View style={styles.container}>
