fcf17d0f325c1afec2a73789f36803d6593d3b63
Ulises <ulivego@hotmail.com>
2022-04-19 08:47:55 -0500

    ref(AppTitle): trying to add color to Lert Text

--- src/components/molecules/AppTitle.tsx
+++ src/components/molecules/AppTitle.tsx
@@ -2,11 +2,12 @@ import { StyleSheet } from 'react-native';
 import LertText from '~components/atoms/LertText';
 import { Box } from 'native-base';
 import theme from '~theme/theme';
+import { background } from 'native-base/lib/typescript/theme/styled-system';
 
 const AppTitle = (props: any) => {
     return (
         <Box style={styles.blackBox}>
-            <LertText text="LERT - LABOR EXPENSES RECOVERY TOOL" type="display02"/>
+            <LertText text="LERT - LABOR EXPENSES RECOVERY TOOL" type="display02" color="icons.primaryLDark"/>
         </Box>
     );
 };
@@ -23,7 +24,7 @@ const AppTitleStyles: { [name: string]: StyleType} = {
 
 const styles = StyleSheet.create({
     blackBox: {
-        backgroundColor: "#ff0000",
+        backgroundColor: "#000",
         alignSelf: "flex-end",
         alignContent: "flex-start",
         padding: 10
