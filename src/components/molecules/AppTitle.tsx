35c396a47e35db0b260d58e2fa4ee741c5583078
Ulises <ulivego@hotmail.com>
2022-04-19 09:42:57 -0500

    rref(AppTitle): Added color and style
    Added color and style to the background of the App Title and the text

--- src/components/molecules/AppTitle.tsx
+++ src/components/molecules/AppTitle.tsx
@@ -1,37 +1,18 @@
 import { StyleSheet } from 'react-native';
 import LertText from '~components/atoms/LertText';
 import { Box } from 'native-base';
-import theme from '~theme/theme';
-import { background } from 'native-base/lib/typescript/theme/styled-system';
 
-const AppTitle = (props: any) => {
+const AppTitle = () => {
     return (
-        <Box style={styles.blackBox}>
-            <LertText text="LERT - LABOR EXPENSES RECOVERY TOOL" type="display02" color="icons.primaryLDark"/>
+        <Box 
+            backgroundColor={"dark.backdrop1"} 
+            alignSelf={"flex-end"}
+            alignContent={"flex-start"}
+            padding={10}
+        >
+            <LertText text="LERT - LABOR EXPENSES RECOVERY TOOL" type="display02" color="components.selectedState"/>
         </Box>
     );
 };
-
-type StyleType = {
-    backgroundColor?: string;
-}
-
-const AppTitleStyles: { [name: string]: StyleType} = {
-    blackBox: {
-        backgroundColor: "dark.backdrop1",
-    }
-}
-
-const styles = StyleSheet.create({
-    blackBox: {
-        backgroundColor: "#000",
-        alignSelf: "flex-end",
-        alignContent: "flex-start",
-        padding: 10
-    },
-    text: {
-        color: "actions.actionPrimary"
-    }
-});
 
 export default AppTitle;
