55f41f65cb26b047f59deb7733b528f80981589e
GSLeonardo <leo_galindo_01@hotmail.com>
2022-04-26 10:58:45 -0500

    ref(fixedHeadings): Styles refactor DEV-101
    - Styles for items in drawer

--- src/styles/fixedHeadings.tsx
+++ src/styles/fixedHeadings.tsx
@@ -9,12 +9,12 @@ const FixedHeadingStyles: { [key: string]: TextType } = {
         lineHeight: 18,
         letterSpacing: 0.16 
     },
-    headingCompact: {
-        fontWeight: "regular",
+    heading2: {
+        fontWeight: "semi-bold",
         fontStyle: "normal",
         fontFamily: "body",
-        fontSize: 14,
-        lineHeight: 18,
+        fontSize: 16,
+        lineHeight: 24,
         letterSpacing: 0.16
     },
     body02Layout: {
@@ -35,6 +35,14 @@ const FixedHeadingStyles: { [key: string]: TextType } = {
     },
     longLayout2: {
         fontWeight: "regular",
+        fontStyle: "normal",
+        fontFamily: "body",
+        fontSize: 32,
+        lineHeight: 40,
+        letterSpacing: 0
+    },
+    boldLongLayout2: {
+        fontWeight: "bold",
         fontStyle: "normal",
         fontFamily: "body",
         fontSize: 32,
