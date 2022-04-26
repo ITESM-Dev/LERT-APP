9a30b33b8786f43b5b2921881cbe1f9343c56b2a
GSLeonardo <leo_galindo_01@hotmail.com>
2022-04-26 10:58:05 -0500

    ref(ProfileInfo): Prop Modifications DEV-101

--- src/components/molecules/ProfileInfo.tsx
+++ src/components/molecules/ProfileInfo.tsx
@@ -1,5 +1,12 @@
-import { Text, Image, Center } from 'native-base';
+
+import { View } from 'react-native';
+import { Image, Box, Pressable } from 'native-base';
+
+import Ionicons  from '@expo/vector-icons/Ionicons';
+
 import LertText from '~components/atoms/LertText';
+import { TouchableOpacity } from 'react-native';
+import theme from '~theme/theme';
 
 type ProfielInfoPropTypes = {
     imagePath?: string;
@@ -14,17 +21,79 @@ type ProfielInfoPropTypes = {
  */
 const ProfileInfo = (props: ProfielInfoPropTypes) => {
     return (
-        <Center>
+        <View
+            style={{
+                alignItems: 'center'
+            }}
+        >
+            {/** Background Image */}
             <Image 
-                size={100}
-                borderRadius={100}
+                width={'100%'}
+                height={'30%'}
+                zIndex={0}
+                justifySelf='flex-start'
+                position='absolute'
+                source={require('~../assets/bgLogin.jpg')}
+                alt="Profile Background"
+            />
+            {/** User Profile Picture */}
+            <Image 
+                {...profileInfoStyle}
                 source={{uri: props.imagePath ? props.imagePath : "https://wallpaperaccess.com/full/317501.jpg" }}
                 alt="testing"
             />
-            <LertText text={props.name} type="body02Layout"/>
-            <LertText text={props.role} type="shortParagraph"/>
-        </Center>
+
+            {/** User Information */}
+            <Box 
+                zIndex={2} 
+                margin={2}
+                justifyContent='center'
+            >
+                <LertText 
+                    text={props.name} 
+                    color={theme.colors.actions.highContrast}
+                    type="heading2"
+                />
+                <LertText 
+                    text={props.role}
+                    color={theme.colors.components.placeholderActive} 
+                    type="paragraphComponents"
+                />
+            </Box>
+
+            {/** Logout Button */}
+            <Pressable
+                width='25%'
+                margin={2}
+                justifyContent='space-around'
+                flexDirection='row'
+                alignItems='center'
+                onPress={() => {alert('Logout')}}
+            >
+                
+                    <LertText 
+                        text='Logout' 
+                        type={'heading'}
+                        color={theme.colors.actions.highContrast}
+                    />
+                    <Ionicons 
+                        name='arrow-forward-outline' 
+                        size={14}
+                        color={theme.colors.actions.highContrast}
+                    />
+
+            </Pressable>
+        </View>
     );
 };
+
+const profileInfoStyle = {
+    size: 100,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: theme.colors.components.offWhite,
+    marginTop: 2,
+    zIndex: 1,
+}
 
 export default ProfileInfo;
