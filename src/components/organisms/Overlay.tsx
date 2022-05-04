aae2a5aebe16cf77a8a9d85be368e4dcfee7e055
Ulises <ulivego@hotmail.com>
2022-05-04 09:13:46 -0500

    ref(Overlay): Created the Overlay
    Created the Overlay component, and added it on the ExtraHours screen to test it. So far it looks good

---
+++ src/components/organisms/Overlay.tsx
@@ -0,0 +1,47 @@
+import { Text, useWindowDimensions } from 'react-native';
+import { Popover, Box, Button, Modal } from 'native-base';
+
+import LertText from '~components/atoms/LertText';
+import LertButton from '~components/atoms/LertButton';
+import React, { useState } from 'react';
+import LertInput from '~components/molecules/LertInput';
+import { propsFlattener } from 'native-base/lib/typescript/hooks/useThemeProps/propsFlattener';
+import theme from '~theme/theme';
+
+type OverlayPropTypes = {
+    children: JSX.Element;
+    maxWidth: string | number;
+    maxHeight: string | number;
+}
+
+const Overlay = (props: OverlayPropTypes) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const initialFocusRef = React.useRef(null);
+
+    return (
+        <Box alignSelf={"flex-end"} paddingRight="10%">
+            {/** 
+             * <Modal initialFocusRef={initialFocusRef} trigger={triggerProps => {
+            return <LertButton {...triggerProps} title="Add extra hour" type="primary" onPress={() => setIsOpen(true)} />;
+            }} isOpen={isOpen} onClose={() => setIsOpen(!isOpen)}>
+            */}
+
+            <LertButton title="Add extra hour" type="primary" onPress={() => setIsOpen(true)} />;
+            <Modal initialFocusRef={initialFocusRef} isOpen={isOpen} onClose={() => setIsOpen(!isOpen)}>
+                <Modal.Content maxWidth={props.maxWidth} maxHeight={props.maxHeight} style={{backgroundColor:theme.colors.text.white}}>
+                    <Modal.CloseButton onPress={() => setIsOpen(false)} />
+
+                    <Modal.Body style={{marginTop:"10%"}}>
+                        {props.children}
+                    </Modal.Body>
+                    <Modal.Footer style={{backgroundColor:theme.colors.text.white, paddingRight:"4%", paddingTop:"4%"}}>
+                        <LertButton title="SUBMIT" type="primary" onPress={() => setIsOpen(false)}/>
+                    </Modal.Footer>
+                </Modal.Content>
+            </Modal>
+        </Box>
+    )
+};
+
+export default Overlay;
--- src/screens/ContentDrawer/ExtraHours.tsx
+++ src/screens/ContentDrawer/ExtraHours.tsx
@@ -1,7 +1,21 @@
 import { Text, View } from "react-native";
 
 import Table from "~components/organisms/Table";
-import TableItem from "~components/molecules/TableItem";
+import LertText from '~components/atoms/LertText';
+import Overlay from '~components/organisms/Overlay';
+import LertInput from '~components/molecules/LertInput';
+import * as textTypes from '~styles/constants/textTypes';
+
+import Theme from '../../theme/theme';
+import { useState } from "react";
+import { HStack, VStack } from "native-base";
+import Dropdown from "~components/molecules/Dropdown";
+
+const dropdownItems = [
+    { label: 'México', value: 'mexico' },
+    { label: 'USA', value: 'usa' },
+    { label: 'India', value: 'india' },
+]
 
 const ExtraHours = () => {
 
@@ -10,9 +24,44 @@ const ExtraHours = () => {
         {Type: 2, Band: 2, Country: "Culiacan", Rate: 4, DateOfStart:"22/03/18", DateOfFinish:"22/04/22"},
     ]
 
+    const [type, setType] = useState("");
+    const [band, setBand] = useState("");
+    const [country, setCountry] = useState("");
+    const [rate, setRate] = useState("");
+    const [dateStart, setDateStart] = useState("");
+    const [dateFinish, setDateFinish] = useState("");
+
     return (
         <View>
-            <Text>ExtraHours</Text>
+
+            <LertText text="Extra Hours" type={textTypes.display04} color={Theme.colors.text.primary} style={{paddingLeft:"10%", paddingTop:"6%"}}/>
+
+            <Overlay maxWidth={"50%"} maxHeight={"50%"}> 
+                <>
+                    <HStack space={2} justifyContent="space-evenly">
+                        <VStack alignItems={"flex-start"}>
+                            <LertText text="Country" type={textTypes.heading} color={Theme.colors.text.primary}/>
+                            <LertInput text={type} setText={setType} placeholder={"Country"}/>
+                            <LertText text="Rate" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
+                            <LertInput text={rate} setText={setRate} placeholder={"Rate"}/>
+                            <LertText text="Type" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
+                            <Dropdown placeholder="Type" items={dropdownItems}/>
+                        </VStack>
+                        <VStack alignItems={"flex-start"}>
+                            <LertText text="Band" type={textTypes.heading} color={Theme.colors.text.primary}/>
+                            <LertInput text={band} setText={setBand} placeholder={"Band"}/>
+                            <LertText text="Start Date" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
+                            <LertInput text={dateStart} setText={setDateStart} placeholder={"Date of Start"}/>
+                            <LertText text="Finish Date" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
+                            <LertInput text={dateFinish} setText={setDateFinish} placeholder={"Date of Finish"}/>
+                        </VStack>
+                    </HStack>
+                </>
+
+            </Overlay>
+
+            <LertText text="Extra Hours List" type={textTypes.display01} color={Theme.colors.text.primary} style={{paddingLeft:"10%", paddingTop:"4%"}}/>
+
             <Table headers={["Type", "Band", "Country", "Rate", "Date of Start", "Date of Finish"]} items={example} flexValues={[1, 1, 2, 1, 2, 2]}/>
         </View>
     )
