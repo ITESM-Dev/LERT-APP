d4cb1646c0a67b4aa62acdb1837332526aa84860
AbigailVelasco <79066523+AbigailVelasco@users.noreply.github.com>
2022-05-05 01:07:23 -0500

    ref(homeScreen):
    Made Home Screen DEV-78

--- src/screens/ContentDrawer/Home.tsx
+++ src/screens/ContentDrawer/Home.tsx
@@ -1,9 +1,114 @@
 import { Text, View } from "react-native";
+import LertText from "~components/atoms/LertText";
+import Theme from '~theme/theme';
+import * as textTypes from '~styles/constants/textTypes';
+import { Box, Center, ScrollView, SimpleGrid } from "native-base";
+import { AspectRatio, Image } from "native-base";
+import {ViewStyle } from 'react'
+import LegalMenu from "~components/molecules/LegalMenu";
+
+type BgBoxPropTypes = {
+    text: string;
+    style?: ViewStyle;
+    textType?: string;
+   // width: string;
+    height?: number;
+}
+
+const BgBox = (props: BgBoxPropTypes) => {
+    return (
+        <Box
+            {...props.style}
+            bgColor={Theme.colors.components.offWhite} 
+            alignItems="left"
+            height="250"
+            padding="5"
+            marginRight={5}
+        >
+            <LertText 
+                text={props.text} 
+                type={textTypes.heading4} 
+                numberOfLines={2}
+                color={Theme.colors.text.primary}
+            />
+        </Box>
+    );
+};
+
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <ScrollView h="500">    
+            <View style={{
+                    width: 1150, height: 150,flexDirection: 'column', marginHorizontal: 70
+                }}>
+
+                <Box style={{marginTop: 80, marginBottom: 20}}>
+                    <LertText
+                    text="Hi user, welcome to LERT"
+                    type={textTypes.display04}
+                    />
+                </Box>
+
+                <Box style={{width:"90%", marginBottom:100 }}>
+                    <LertText
+                        text="This is an aid in the finances of the manager,it 
+                                manages squad, employees and create the fall plan planning
+                                expenses and financial recoveries."
+                        type={textTypes.display03}
+                        numberOfLines={20}
+                        />
+                </Box>
+
+                <Box style={{width:"90%", marginBottom:50 }}>
+                    <LertText
+                        text="The goals of this application are:"
+                        type={textTypes.heading4}
+                        numberOfLines={5}
+                        />
+                </Box>
+                
+                <View  style={{ width: 1150, height: 500,flexDirection: 'column'}}>
+                    <View style={{ width: 1150,marginBottom:30, flexDirection: 'row'}}>
+                        <BgBox 
+                            text="Reduction of time invested by managers" 
+                            style={{width:"55%"}}/>
+                        
+                        <BgBox 
+                            text="Reduction of time invested by managers" 
+                            style={{width:"30%"}}/>
+                    </View>
+                    <View style={{ width: 1150,flexDirection: 'row', marginBottom: 30}}>
+                        <BgBox 
+                            text="Reduction of time invested by managers" 
+                            style={{width:"30%"}}/>
+                        
+                        <BgBox 
+                            text="Reduction of time invested by managers" 
+                            style={{width:"30%"}}/>
+                    </View>
+
+                    <View style={{ width: 1150, flexDirection: 'row', marginBottom: 30}}>
+                        <BgBox 
+                            text="Reduction of time invested by managers" 
+                            style={{width:"30%"}}/>
+                        
+                        <Box style={{backgroundColor:"Blue", paddingLeft: 20}}>
+                            <Center>
+                                <Image 
+                                    source={require('~../assets/bee.webp')}
+                                    alt="IBM Bee" size="2xl" />
+                            </Center>
+                        
+                        </Box>
+                    </View>
+                
+
+                
+                </View>
+            </View>
+        </ScrollView>
+        
+        
     )
 };
 
