1f0c2829f5ba71060086c0b07e443da0f0af395a
GSLeonardo <leo_galindo_01@hotmail.com>
2022-05-19 16:58:36 -0500

    ref)Expense Type): missing info DEV-126

--- src/store/expenses/types.ts
+++ src/store/expenses/types.ts
@@ -1,4 +1,14 @@
 export type ExpenseType = {
     id: string;
     type: string;
+    const: number;
+    date: string;
+    comment: string;
+    ICA: string;
+    idICA?: string;
+    employeeMail: string;
+    idICAManager?: string;
+    ICAManager: string;
+    idAdministrator?: string;
+    administrator: string;
 }
