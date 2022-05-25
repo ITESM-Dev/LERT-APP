5b3100145de8ad1fe957e59a78ba9eda5db9185a
GSLeonardo <leo_galindo_01@hotmail.com>
2022-05-25 11:31:07 -0500

    ref(test): Dummy test file deleted DEV-165

--- tests/dummy.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react-native'
-import LertButton from '~components/atoms/LertButton'
-
-test("Try test 2 + 2", () => {
-    render(<LertButton title='test' type='primary' onPress={() => console.log("Test")}/>); 
-    expect(2 + 2).toBe(4)
-})
