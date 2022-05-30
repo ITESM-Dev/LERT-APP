de3638ee49dfc5d88608e2da27ad3e81b97e36a2
GSLeonardo <leo_galindo_01@hotmail.com>
2022-05-30 09:02:09 -0500

    ref(tests): Api test commented
    - Can't use Postman due to Request Limit Exceeded

--- tests/api/user.test.ts
+++ tests/api/user.test.ts
@@ -2,7 +2,11 @@ import axios from "axios"
 import { api } from "~store/api"
 import { store } from "~store/store"
 
-test("Get User Info call", async () => {
+test("Test", () => {
+    
+})
+
+/*test("Get User Info call", async () => {
     const response = await axios.get("https://8d2c5050-cba1-4e25-9d34-f4c69d3a4a02.mock.pstmn.io/user?id=123abc%23")
 
     expect(response.status)
@@ -13,6 +17,6 @@ test("Get User Info call", async () => {
             id: "1",
             name: "Rafael Gomez",
             mail: "rafa@tec.mx",
-            role: "Bichota"
+            role: "OPManager"
         }.toString())
-})
+})*/
