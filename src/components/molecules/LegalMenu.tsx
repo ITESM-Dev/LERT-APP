3f421176884810a379012e73afc2ae6338e826bd
GSLeonardo <leo_galindo_01@hotmail.com>
2022-05-30 13:12:02 -0500

    ref(Legal Menu): Links added to Menu

--- src/components/molecules/LegalMenu.tsx
+++ src/components/molecules/LegalMenu.tsx
@@ -1,13 +1,12 @@
 import { Box, Center, Image } from 'native-base'
-import { View } from 'react-native';
-import Theme from '~theme/theme';
 import LertText from '~components/atoms/LertText'
+import Theme from '~theme/theme';
+import { handleOpenUrl } from '~utils/handlers';
 
 /**
  * Bottom Menu containing links to different legal information sites
  */
 const LegalMenu = () => {
-
 
     return (
         <Box style={{flexDirection:"column"}}>
@@ -15,64 +14,56 @@ const LegalMenu = () => {
                 bgColor={Theme.colors.icons.secondary}     
                 width={'100%'} 
                 height={'15%'}
+            />
+        
+            <Box 
+                bgColor={Theme.colors.components.highContrast}     
+                width={'100%'} 
                 display='flex' 
                 flexDirection='column'
                 justifyContent='space-around'
-                padding={3}>
-             </Box>
-        
-        <Box 
-            bgColor={Theme.colors.components.highContrast}     
-            width={'100%'} 
-            display='flex' 
-            flexDirection='column'
-            justifyContent='space-around'
-            padding={3}
-            paddingLeft={70}
-            paddingTop={'3%'}
-            paddingBottom={'3%'}
-        >
-            <LertText 
-                type= 'body02Layout'
-                color= {Theme.colors.text.white}
-                text='Terms of Use' 
-                onPress={() => alert("Navigate to Terms of User")}
-            />
-            <LertText 
-                type= 'body02Layout'
-                color= {Theme.colors.text.white}
-                text='IBM Internal Privacy Statement' 
-                onPress={() => alert("IBM Internal Privacy Statement")}
-            />
-            <LertText 
-                type= 'body02Layout'
-                color= {Theme.colors.text.white}
-                text='Feedback' 
-                onPress={() => alert("Feedback")}
-            />
-            <LertText 
-                type= 'body02Layout'
-                color= {Theme.colors.text.white}
-                text='Bussiness Conduct Guidelines' 
-                onPress={() => alert("Bussiness Conduct Guidelines")}
-            />
-            <LertText 
-                type= 'body02Layout'
-                color= {Theme.colors.text.white}
-                text='Contact Support' 
-                onPress={() => alert("Contact Support")}
-            />
-            <Box style={{width:'100%'}}>
-                <Image 
-                    resizeMode={"contain"} 
-                    //borderRadius={100} 
-                     source={require('~../assets/ibm-white-logo.png')}
-                     alt="IBM Bee" size="xs" />
-                
-                            
-             </Box>
+                paddingTop={'3%'}
+                paddingX={'5%'}
+            >
+                <LertText 
+                    type= 'body02Layout'
+                    color= {Theme.colors.text.white}
+                    text='Terms of Use' 
+                    onPress={() => handleOpenUrl('https://www.ibm.com/legal')}
+                />
+                <LertText 
+                    type= 'body02Layout'
+                    color= {Theme.colors.text.white}
+                    text='IBM Internal Privacy Statement' 
+                    onPress={() => handleOpenUrl("https://www.ibm.com/us-en/privacy")}
+                />
+                <LertText 
+                    type= 'body02Layout'
+                    color= {Theme.colors.text.white}
+                    text='Feedback' 
+                    onPress={() => handleOpenUrl("https://www.ibm.com/community/feedback/")}
+                />
+                <LertText 
+                    type= 'body02Layout'
+                    color= {Theme.colors.text.white}
+                    text='Business Conduct Guidelines' 
+                    onPress={() => handleOpenUrl("https://www.ibm.com/investor/governance/business-conduct-guidelines")}
+                />
+                <LertText 
+                    type= 'body02Layout'
+                    color= {Theme.colors.text.white}
+                    text='Contact Support' 
+                    onPress={() => handleOpenUrl("https://www.ibm.com/mysupport/s/?language=en_US")}
+                />
+                <Box style={{ width:'100%' }}>
+                    <Image 
+                        resizeMode={"contain"} 
+                        source={require('~../assets/ibm-white-logo.png')}
+                        alt="IBM Logo" size="xs" />
+                     
+                </Box>
 
-        </Box>
+            </Box>
         </Box>
 
     )
