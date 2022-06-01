75e4bb07b0a2387c3a31499361af72636d46bb93
GSLeonardo <leo_galindo_01@hotmail.com>
2022-06-01 12:14:19 -0500

    feat(utils): API URL constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+const LOCAL_URL = "http://localhost:8000/" 
+const DEV_URL = ""
+const PROD_URL = ""
+
+export const API_URL =  LOCAL_URL
--- tests/api/user.test.ts
+++ tests/api/user.test.ts
@@ -2,12 +2,14 @@ import axios from "axios"
 import { api } from "~store/api"
 import { store } from "~store/store"
 
+import { API_URL } from "~utils/constants"
+
 test("Test", () => {
     
 })
 
 /*test("Get User Info call", async () => {
-    const response = await axios.get("https://8d2c5050-cba1-4e25-9d34-f4c69d3a4a02.mock.pstmn.io/user?id=123abc%23")
+    const response = await axios.get(API_URL + "user?id=123abc%23")
 
     expect(response.status)
         .toBe(200)
