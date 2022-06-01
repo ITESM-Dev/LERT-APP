d1a46224cf0796ed7bce7b6b23154a03274d9433
GSLeonardo <leo_galindo_01@hotmail.com>
2022-06-01 12:15:03 -0500

    build(Dependencies): AsyncStorage Library

--- Main.tsx
+++ Main.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
 import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+
+import Content from "Content";
+
 import AppStack from "~navigators/AppStack";
+
+import { AppDispatch } from "~store/store";
+import { 
+    getTokenFromStorageThunk, 
+    setUser, 
+    userSelector, 
+    UserType
+} from "~store/user";
 
 const Main = () => {
 
@@ -8,11 +21,30 @@ const Main = () => {
         prefixes: [ 'lertApp://' ] // Not used for web
     }
 
-    return (
+    const dispatch: AppDispatch = useDispatch()
+    const user = useSelector(userSelector)
+
+    /** @todo Add Token verification to get User Info from API */
+    useEffect(() => {
+        dispatch(getTokenFromStorageThunk()).then(({ payload }: any) => {
+            if (payload !== null) 
+                dispatch(setUser({ token: payload } as UserType))
+        })
+    }, [])
+
+    if (Object.keys(user).length === 0 || user.token === "undefined") 
+        return (
+            <NavigationContainer linking={linkingOptions}>
+                <AppStack/>
+            </NavigationContainer>
+        )
+    
+    return (  
         <NavigationContainer linking={linkingOptions}>
-            <AppStack/>
+            <Content />
         </NavigationContainer>
     )
+    
 };
 
 export default Main;
