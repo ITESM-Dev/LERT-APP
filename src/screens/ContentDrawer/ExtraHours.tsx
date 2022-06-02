dc7765ff686e415dc82ed813f3682517cbcdd251
GSLeonardo <leo_galindo_01@hotmail.com>
2022-06-02 11:05:33 -0500

    feat(Extra Hours): Call for Create Extra Hour Endpoint DEV-203

--- src/screens/ContentDrawer/ExtraHours.tsx
+++ src/screens/ContentDrawer/ExtraHours.tsx
@@ -15,15 +15,10 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { allExtraHours } from "~store/extraHours/selectors";
 import LertScreen from "~components/organisms/LertScreen";
+import { ExtraHourForm, useCreateExtraHourTypeMutation } from "~store/api";
 
 const dropdownCountries = [
-    { label: 'México', value: 'mexico' },
-    { label: 'India', value: 'india' },
-]
-
-const dropdownTypes = [
-    { label: 'First', value: 'first' },
-    { label: 'Secondary', value: 'secondary' },
+    { label: 'México', value: 'Mexico' },
 ]
 
 const TABLE_HEADERS = ["Type", "Band", "Country", "Rate", "Date of Start", "Date of Finish"]
@@ -46,31 +41,108 @@ const ExtraHours = () => {
     const dispatch: AppDispatch = useDispatch();
 
     // Extra Hours - State
-    const expenses = useSelector(allExtraHours);
+    const extraHours = useSelector(allExtraHours);
+
+    // API Calls
+    const [createExtraHourType, response] = useCreateExtraHourTypeMutation()
+
+    const handleSubmit = () => {
+
+        const extraHourForm: ExtraHourForm = {
+            type: type,
+            band: band,
+            country: country,
+            rate: rate,
+            dateToStart: dateStart,
+            dateToFinish: dateFinish,
+        }
+
+        createExtraHourType(extraHourForm).unwrap()
+            .then(payload => alert("Nice"))
+            .catch(error => alert("Error"))
+    }
 
     return (
         <LertScreen>
 
             <LertText text="Extra Hours" type={textTypes.display04} color={Theme.colors.text.primary} style={{paddingLeft:"10%", paddingTop:"6%"}}/>
 
-            <Overlay maxWidth={"50%"} maxHeight={"50%"} buttonTitle="Add Extra Hour"> 
+            <Overlay 
+                maxWidth={"50%"}
+                maxHeight={"50%"} 
+                buttonTitle="Add Extra Hour"
+                handleSubmit={handleSubmit}
+            > 
                 <>
                     <HStack space={2} justifyContent="space-evenly">
                         <VStack alignItems={"flex-start"}>
-                            <LertText text="Country" type={textTypes.heading} color={Theme.colors.text.primary}/>
-                            <Dropdown placeholder="Country" items={dropdownCountries}/>
-                            <LertText text="Rate" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
-                            <LertInput text={rate} setText={setRate} placeholder={"Rate"}/>
-                            <LertText text="Type" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
-                            <Dropdown placeholder="Type" items={dropdownTypes}/>
+                            <LertText 
+                                text="Country" 
+                                type={textTypes.heading} 
+                                color={Theme.colors.text.primary}
+                            />
+                            <Dropdown 
+                                placeholder="Country" 
+                                items={dropdownCountries}
+                                value={country}
+                                setValue={setCountry}
+                            />
+                            <LertText 
+                                text="Rate" 
+                                type={textTypes.heading} 
+                                color={Theme.colors.text.primary} 
+                                style={{ paddingTop:"10%" }}
+                            />
+                            <LertInput 
+                                text={rate} 
+                                setText={setRate} 
+                                placeholder={"Rate"}
+                            />
+                            <LertText 
+                                text="Type" 
+                                type={textTypes.heading} 
+                                color={Theme.colors.text.primary} 
+                                style={{ paddingTop:"10%" }}
+                            />
+                            <LertInput 
+                                text={type} 
+                                setText={setType} 
+                                placeholder={"Type"}
+                            />
                         </VStack>
                         <VStack alignItems={"flex-start"}>
-                            <LertText text="Band" type={textTypes.heading} color={Theme.colors.text.primary}/>
-                            <LertInput text={band} setText={setBand} placeholder={"Band"}/>
-                            <LertText text="Start Date" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
-                            <LertInput text={dateStart} setText={setDateStart} placeholder={"Date of Start"}/>
-                            <LertText text="Finish Date" type={textTypes.heading} color={Theme.colors.text.primary} style={{paddingTop:"10%"}}/>
-                            <LertInput text={dateFinish} setText={setDateFinish} placeholder={"Date of Finish"}/>
+                            <LertText 
+                                text="Band" 
+                                type={textTypes.heading} 
+                                color={Theme.colors.text.primary}
+                            />
+                            <LertInput 
+                                text={band} 
+                                setText={setBand} 
+                                placeholder={"Band"}
+                            />
+                            <LertText 
+                                text="Start Date" 
+                                type={textTypes.heading} 
+                                color={Theme.colors.text.primary} 
+                                style={{paddingTop:"10%"}}
+                            />
+                            <LertInput 
+                                text={dateStart} 
+                                setText={setDateStart} 
+                                placeholder={"Date of Start"}
+                            />
+                            <LertText 
+                                text="Finish Date" 
+                                type={textTypes.heading} 
+                                color={Theme.colors.text.primary} 
+                                style={{paddingTop:"10%"}}
+                            />
+                            <LertInput 
+                                text={dateFinish} 
+                                setText={setDateFinish} 
+                                placeholder={"Date of Finish"}
+                            />
                         </VStack>
                     </HStack>
                 </>
