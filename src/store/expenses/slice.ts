b7682f481c4cbb396307faeb8b60c9ea805cea3f
GSLeonardo <leo_galindo_01@hotmail.com>
2022-06-05 20:55:48 -0500

    feat(Expenses Slice): Matcher for Get Expenses

--- src/store/expenses/slice.ts
+++ src/store/expenses/slice.ts
@@ -1,5 +1,5 @@
-import { createAction, createEntityAdapter, createSlice, EntityId, PayloadAction } from "@reduxjs/toolkit";
-import produce from "immer";
+import { createEntityAdapter, createSlice, EntityId, PayloadAction } from "@reduxjs/toolkit";
+import { api } from "~store/api";
 
 import { ExpenseType } from "./types";
 
@@ -37,6 +37,15 @@ const expensesSlice = createSlice({
             expensesAdapter.removeAll(state);
         }
     },
+    extraReducers: (builder) => {
+        builder
+            .addMatcher(
+                api.endpoints.getExpenses.matchFulfilled,
+                (state, { payload }) => {
+                    expensesAdapter.setMany(state, payload)
+                }
+            )
+    }
 });
 
 export const {
