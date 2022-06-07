e904996df5f94f9a50f2409d4338452bd6621cce
GSLeonardo <leo_galindo_01@hotmail.com>
2022-06-07 13:10:46 -0500

    feat(User Roles screen): Get and Update Role Endpoints DEV-246

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,3 +15,11 @@ export enum USER_ROLES {
     ADMIN = "Admin",
     RESOURCE = "Resource"
 }
+
+export const USER_ROLES_ARRAY = [
+    { label: USER_ROLES.ADMIN, value: USER_ROLES.ADMIN },
+    { label: USER_ROLES.OP_MANAGER, value: USER_ROLES.OP_MANAGER },
+    { label: USER_ROLES.ICA_ADMIN, value: USER_ROLES.ICA_ADMIN },
+    { label: USER_ROLES.MANAGER, value: USER_ROLES.MANAGER },
+    { label: USER_ROLES.RESOURCE, value:USER_ROLES.RESOURCE },
+]
