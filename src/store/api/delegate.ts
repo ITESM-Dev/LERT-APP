77f44fb2294eb113b84bff43db313d736c7fcab7
Ulises <ulivego@hotmail.com>
2022-06-08 18:29:58 -0500

    made som changes
    Sorry for the dirty commit

--- src/store/api/delegate.ts
+++ src/store/api/delegate.ts
@@ -1,5 +1,6 @@
 import { ManagerType } from "~store/managers";
-import { BuilderType, validateGetStatus, validateUpdateStatus } from "./slice";
+import { BuilderType, validateGetStatus, validatePostStatus, validateUpdateStatus } from "./slice";
+import { IcaAdminType, ManagerIcaAdminType } from "./types";
 
 
 export const getManagersIcaAdmin = (builder: BuilderType) => (
@@ -24,6 +25,72 @@ export const getManagersIcaAdmin = (builder: BuilderType) => (
         }
     })
 )
+
+export const getManagersAndIcaAdmins = (builder: BuilderType) => (
+    builder.query<ManagerIcaAdminType[], void>({
+       query: () => ({
+           url: "getManagersAndIcaAdmins",
+           validateStatus: validateGetStatus,
+       }),
+       providesTags: ["Delegate"],
+       transformResponse: (response) => {
+           const adminManagersAPI = response as ManagerIcaAdminType[]
+
+           const adminManagers = adminManagersAPI.map(item => ({
+               managerMail: item.managerMail,
+               icaAdminMail: item.icaAdminMail,
+           }))
+
+           return adminManagers;
+       }
+    })
+)
+
+export const getIcaAdmins = (builder: BuilderType) => (
+    builder.query<IcaAdminType[], void>({
+        query: () => ({
+            url: "getIcaAdmins",
+            validateStatus: validateGetStatus,
+        }),
+        providesTags: ["Delegate"],
+        transformResponse: (response) => {
+            const icaAdminsAPI = response as IcaAdminType[]
+ 
+            const icaAdmins = icaAdminsAPI.map(item => ({
+                mail: item.mail,
+            }))
+ 
+            return icaAdmins;
+        }
+    })
+)
+
+export const opAssignIcaAdminManager = (builder: BuilderType) => (
+    builder.mutation<void, ManagerIcaAdminType>({
+        query: (ManagerIcaAdminType) => ({
+            url: "opAssignIcaAdminManager",
+            method: "POST",
+            body: ManagerIcaAdminType,
+            responseHandler: "json",
+            validateStatus: validatePostStatus
+        }),
+        invalidatesTags: ["Delegate"],
+    })
+)
+
+export const setIcaAdmin = (builder: BuilderType) => (
+    builder.mutation<void, IcaAdminType>({
+        query: (IcaAdminType) => ({
+            url: "setIcaAdmin",
+            method: "POST",
+            body: IcaAdminType,
+            responseHandler: "json",
+            validateStatus: validatePostStatus
+        }),
+        invalidatesTags: ["Delegate"],
+    })
+)
+
 
 /*
 export const updateManagersIcaAdmin = (builder: BuilderType) => (
